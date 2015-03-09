b2e474fae5f4e463206b5c0d93330cfede1810de
Tuan Nguyen <anhtuan8591@gmail.com>
2015-03-09 17:04:08 +0700

    First commit

---
+++ admin/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/diencoviet/admin/');
+define('HTTP_CATALOG', 'http://localhost/diencoviet/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/diencoviet/admin/');
+define('HTTPS_CATALOG', 'http://localhost/diencoviet/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/html/diencoviet/admin/');
+define('DIR_SYSTEM', '/var/www/html/diencoviet/system/');
+define('DIR_LANGUAGE', '/var/www/html/diencoviet/admin/language/');
+define('DIR_TEMPLATE', '/var/www/html/diencoviet/admin/view/template/');
+define('DIR_CONFIG', '/var/www/html/diencoviet/system/config/');
+define('DIR_IMAGE', '/var/www/html/diencoviet/image/');
+define('DIR_CACHE', '/var/www/html/diencoviet/system/cache/');
+define('DIR_DOWNLOAD', '/var/www/html/diencoviet/system/download/');
+define('DIR_UPLOAD', '/var/www/html/diencoviet/system/upload/');
+define('DIR_LOGS', '/var/www/html/diencoviet/system/logs/');
+define('DIR_MODIFICATION', '/var/www/html/diencoviet/system/modification/');
+define('DIR_CATALOG', '/var/www/html/diencoviet/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'diencoviet');
+define('DB_PREFIX', 'oc_');
---
+++ config.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/diencoviet/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/diencoviet/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/html/diencoviet/catalog/');
+define('DIR_SYSTEM', '/var/www/html/diencoviet/system/');
+define('DIR_LANGUAGE', '/var/www/html/diencoviet/catalog/language/');
+define('DIR_TEMPLATE', '/var/www/html/diencoviet/catalog/view/theme/');
+define('DIR_CONFIG', '/var/www/html/diencoviet/system/config/');
+define('DIR_IMAGE', '/var/www/html/diencoviet/image/');
+define('DIR_CACHE', '/var/www/html/diencoviet/system/cache/');
+define('DIR_DOWNLOAD', '/var/www/html/diencoviet/system/download/');
+define('DIR_UPLOAD', '/var/www/html/diencoviet/system/upload/');
+define('DIR_MODIFICATION', '/var/www/html/diencoviet/system/modification/');
+define('DIR_LOGS', '/var/www/html/diencoviet/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'diencoviet');
+define('DB_PREFIX', 'oc_');
